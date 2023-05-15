0064768ad970bc820b86d1c488446ff3b21d67c5
alizoort <alizoort94@gmail.com>
2023-05-15 18:28:27 +0300

    logout button

--- src/app/landing/home/home.component.ts
+++ src/app/landing/home/home.component.ts
@@ -5,19 +5,23 @@ import { BehaviorSubject, Observable } from 'rxjs';
 import { AuthService } from 'src/app/shared/services/auth.service';
 import { SpinnerService } from 'src/app/shared/services/spinner.service';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  constructor(public camundaService: CamundaService,public authService:AuthService,public spinnerService: NgxSpinnerService) { 
+  constructor(public camundaService: CamundaService,public authService:AuthService,public spinnerService: NgxSpinnerService,private router:Router) { 
   }
-  opened: boolean=true;
+  opened: boolean=false;
 
   ngOnInit(): void {
   }
   ngAfterViewInit(){
+  }
+  logout(event){
+    this.router.navigate(["/auth/login"])
   }
 
 }
